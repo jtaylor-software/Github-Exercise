6c09db9e07a72eac58c845019043c59f4baba1ca
Jeremy Taylor <jtaylor@jctaylor.org>
2023-05-13 11:36:20 -0500

    Initial Commit

---
+++ Github Exercise/Github_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExerciseApp.swift
+//  Github Exercise
+//
+//  Created by Jeremy Taylor on 5/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Github_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
